7a359934dd9c97a39102303f683a43feacf02353
Yanny Castrillon <yannyandres117@gmail.com>
2018-05-23 09:04:47 -1000

    Build endpoint to show final destination and source of a particular pathway.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -34,17 +34,43 @@ class FlightsController < ApplicationController
   end
 
   def calculate_path
-    # puts params[:flight_path]
+    paths = format_path(params[:flight_path])
+    final = calculate_source_destination(paths)
 
-    @paths = format_path(params[:flight_path])
-    # calculate_source_destination(@paths)
-
-    render json: { "client_path": @paths }
+    render json: { "client_path": final }
   end
 
   private
 
-    # format the input string into an arra data structure.
+    def calculate_source_destination(paths)
+      # [["'ATL'", " 'EWR'"], ["'SFO'", " 'ATL'"]]
+      source_destination = Hash.new
+      # Build hash structure where 'sources' and 'destination'
+      paths.each do |tuple|
+        source_destination[tuple.first] = tuple.last
+      end
+      # Take out all spaces on values
+      source_destination.each_value(&:strip!)
+      validate_final_source_destination(source_destination)
+    end
+
+    def validate_final_source_destination(source_destination)
+      arr_final = Array.new
+      source_destination.keys.each do |src|
+        unless source_destination.value?(src)
+          @src = src
+        end
+      end
+      source_destination.values.each do |destination|
+        unless source_destination.key?(destination)
+          @destination = destination
+        end
+      end
+      arr_final << @src
+      arr_final << @destination
+    end
+
+    # formats the current string to an array of tuples
     def format_path(str)
       first_format = str[1..str.length - 2].split('|') # => [ "['ATL', 'EWR'] ", " ['SFO', 'ATL']" ]
       second_format = first_format.map { |e| e[1..e.length - 2] } # => ["'ATL', 'EWR'", "'SFO', 'ATL'"]
